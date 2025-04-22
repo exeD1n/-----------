8a29537189f391bc3ec314098aab00cbc0995532
Lutaev Daniil Olegovich <daniillutaev1@gmail.com>
2025-04-22 18:02:49 +0400

    ff

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -6,14 +6,23 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите имя',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите фамилию',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите адрес электронной почты',
+            }),
+        }
         
 from django import forms
 from .models import UserAddress
-
-class AddressForm(forms.ModelForm):
-    class Meta:
-        model = UserAddress
-        fields = ['street', 'city', 'state', 'postal_code', 'country']
         
 class AddressForm(forms.ModelForm):
     class Meta:
